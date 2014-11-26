10f79e9debac7bb18c4db695565b54586780b09d
OG <github.com@landroverz.com>
2014-11-26 15:23:57 +0200

    Structuring data before saving and downloading
    Added deep_merge for all format
    Structred font tree for each format and weight

--- css.rb
+++ css.rb
@@ -31,22 +31,29 @@ end; }
 
 
 class CSS
-    @tmp = './tmp_fonts'; ## tmp dir to store fonts
-    @fonts = []; ## fonts list to download
-    @url = {};
+    @tmp = './tmp_fonts' ## tmp dir to store fonts
+    @fonts = [] ## fonts list to download
+    @url = {} ## fonts path.. usually meant for google but sent as constructor args
 
     def initialize(params = {})
         @fonts = params.fetch(:fonts, [])
         @url = params.fetch(:url, {})
     end
 
-    public
     def load
         @fonts.each do |font|
-            getTypes().each do |type, useragent|
-                font_css = getStyle("#{font}", "#{type}")
-                p font_css
+            raw_css = {};
+            getTypes().each do |type, prop|
+                stylesheet = getStyle(font, type)
+                font_details = parseCss(stylesheet, type)
+                raw_css[type] = {
+                    'raw' => stylesheet
+                }
+
+                pp font_details
             end
+
+            puts raw_css
         end
 
         puts 'loading'
@@ -54,13 +61,37 @@ class CSS
 
 
     private
+        ##
+        def parseCss(stylesheet, type)
+            fonts = {} ## parsed fonts data
+            stylesheet.split('@font-face').drop(1).each do |font|
+                family, style, weight = font.scan(/.*font-(weight|style|family): (.*?);/)
+                src = font.scan(/.*url\((.*?)\) format/)
+                family = family.pop.gsub("'", '')
+                weight = weight.pop
+                style = style.pop
+
+                fonts["#{family}#{weight}"] = {
+                    'font-family' => family,
+                    'font-style' => style,
+                    'font-weight' => weight,
+                    'src' => {
+                        type => src
+                    }
+                }
+            end
+
+            return fonts
+        end
+
+
         ## Creates HTTP request and returns CSS string for that user-agent.
         def getStyle(font, type)
             log('info', "Downloading CSS for font: #{font} and Type: #{type}")
 
             http = Net::HTTP.new(@url['domain'])
             req = Net::HTTP::Get.new(@url['path'] + font, {
-                'User-Agent' => getTypes()[type]
+                'User-Agent' => getTypes()[type]['useragent']
             })
             response = http.request(req)
 
@@ -70,17 +101,34 @@ class CSS
         ## Types and useragents mapping
         def getTypes
             return {
-                'eot' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727)',
-                'svg' => 'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
-                'ttf' => 'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
-                'woff' => 'Mozilla/5.0 (Windows NT 5.2; rv:33.0) Gecko/20100101 Firefox/33.0',
-                'woff2' => 'Mozilla/5.0 (Windows NT 5.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
+                'eot' => {
+                    'useragent' => 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727)',
+                    'format' => 'eot'
+                },
+                'ttf' => {
+                    'useragent' => 'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+                    'format' => 'truetype'
+                },
+                'woff' => {
+                    'useragent' => 'Mozilla/5.0 (Windows NT 5.2; rv:33.0) Gecko/20100101 Firefox/33.0',
+                    'format' => 'woff'
+                },
+                'woff2' => {
+                    'useragent' => 'Mozilla/5.0 (Windows NT 5.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
+                    'format' => 'woff2'
+                }
             }
         end
 
         ## Genric way to print verbose
         def log(lvl, text)
             puts "[#{lvl}]: #{text}"
+        end
+
+        ## Deep merge 2 hashes
+        def deep_merge(p)
+            m = proc { |key,v,vv| v.class == Hash && vv.class == Hash ? v.merge(vv, &m) : vv }
+            merge(p, &m)
         end
 end
 
